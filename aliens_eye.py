abdfb42ce50ca375c9f601600a797ec6bd1dfc78
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2021-09-22 13:03:35 +0530

    Update aliens_eye.py

--- aliens_eye.py
+++ aliens_eye.py
@@ -22,6 +22,7 @@ banner=f"""{y}
 os.system("clear")
 print(banner)
 print(f"{r}NOTE:The data may not be compleatly accurate!\n")
+print(f"{r}NOTE: for educational purpose only!\n")
 u=input(f"{y}Enter the username{r}:{g}")
 social={
 "facebook":f"https://facebook.com/{u}",
