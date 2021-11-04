754dd7c8eaaa369ab63c9a4cadb97e930e4b01ef
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2021-11-04 13:19:50 +0000

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+if [[ $(id -u) -ne 0 ]] ; then
+	echo "you need root assess to uninstall the tool"
+	echo
+	echo "so please enter the password to login as root!"
+	sudo bash uninstall.sh
+	exit
+fi
+FILE=/usr/share/aliens_eye.py
+if [ -f "$FILE" ]; then
+	rm /usr/share/aliens_eye.py
+fi
+FILE1=/usr/bin/aliens_eye
+if [ -f "$FILE1" ]; then
+	rm /usr/bin/aliens_eye
+fi
+echo
+echo "uninstalled successfully!"
+echo
