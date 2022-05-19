4c4b9222bd40f273311ed049a545a4ccb56bb372
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2022-05-19 16:58:57 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,43 +3,27 @@ clear
 echo 
 echo
 if [[ $(id -u) -ne 0 ]] ; then
-	echo "you need root assess to install necessary packages"
-	echo
-	echo "so please enter the password to login as root!"
-	echo
+	printf "you need root assess to install necessary packages\n\n"
+
+	printf  "so please enter the password to login as root!\n\n"
+
 	sudo bash ${0}
-	echo 
-	echo 
+	printf "\n\n"
 	exit
 fi
-echo "NOTE: you need internet connction to install necessary packages"
-echo "so please make sure computer is connected to internet"
-echo
-echo
+printf "NOTE: you need internet connction to install necessary packages\n"
+printf "so please make sure computer is connected to internet\n\n"
 for i in 3 2 1
 do
 	echo "staring installation process in ${i}" ; 
 	sleep 1
 done
-echo 
-echo 
+printf "\n\n" 
 apt update -y
 apt install python3 pip -y
 pip3 install requests
-FILE1=/usr/bin/aliens_eye
-if [ -f "$FILE1" ]; then
-	rm /usr/bin/aliens_eye
-fi
-VAR1="${0}"
-VAR2="${0##*/}"
-if [ "$VAR1" = "$VAR2" ]; then
-	cp aliens_eye.py /usr/bin/aliens_eye
-else
-	cp ${0%/*}/aliens_eye.py /usr/bin/aliens_eye
-fi
+rm /usr/bin/aliens_eye &>/dev/null
+cp aliens_eye.py /usr/bin/aliens_eye
 chmod +x /usr/bin/aliens_eye
-echo 
-echo
-echo "installed successfully!"
-echo
-echo "now run command \"aliens_eye\" in terminal!(without \"\")"
+printf "\n\ninstalled successfully!"
+printf "\nnow run command \"aliens_eye\" in terminal!(without \"\")"
