dd70e26ba5d0e453ea104b2c8522030bab427a6f
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2022-05-19 17:42:16 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 clear
 if [[ $(id -u) -ne 0 ]] ; then
 	printf "you need root assess to uninstall the tool\n\n\n"
-	echo "so please enter the password to login as root!\n\n"
+	printf "so please enter the password to login as root!\n\n"
 	sudo bash ${0}
 	exit
 fi
