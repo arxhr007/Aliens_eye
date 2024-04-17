d96e52be6b7dd2593b49abfb0d284378d6dfedc6
Aaron Thomas <84493696+arxhr007@users.noreply.github.com>
2024-04-17 21:08:53 +0530

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -4,7 +4,7 @@ printf "\n\n"
 rm /data/data/com.termux/files/usr/bin/aliens_eye &>/dev/null
 rm /data/data/com.termux/files/usr/bin/sites.json &>/dev/null
 cp aliens_eye.py /data/data/com.termux/files/usr/bin/aliens_eye
-cp aliens_eye.py /data/data/com.termux/files/usr/bin/sites.json
+cp sites.json /data/data/com.termux/files/usr/bin/sites.json
 
 chmod +x /data/data/com.termux/files/usr/bin/aliens_eye
 printf "\n\ninstalled successfully!\n"
