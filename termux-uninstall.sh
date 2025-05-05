6b1dd867ba2df6e9a4004aefdc923eab4ea89b82
Aaron Thomas <84493696+arxhr007@users.noreply.github.com>
2025-05-06 02:09:43 +0530

    Delete termux-uninstall.sh

--- termux-uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-rm /data/data/com.termux/files/usr/bin/aliens_eye &>/dev/null
-rm /data/data/com.termux/files/usr/bin/sites.json &>/dev/null
-
-printf "\nuninstalled successfully!\n"
